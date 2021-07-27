b7f9e4cb6cc4434ba55e5fa517ecd19c053c7e36
etamar <etamar.levy@gmail.com>
2021-07-27 14:53:58 -0400

    commit tree 92da4fafa6d4ec491fbb35d9f81c727e5ff95914

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+
+
+
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagrams)
+        anagrams.select do |word|
+            if @word.split("").sort == word.split("").sort
+                word
+            end
+        end
+    end
+end
